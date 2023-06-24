80cf60a6cf55336bb9fbaf0df773f08927606521
KareemRoshdy <87579367+KareemRoshdy@users.noreply.github.com>
2023-06-24 03:21:18 +0300

    fixing some bugs

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -14,7 +14,7 @@ const cloudinaryUploadImage = async (fileToUpload) => {
     });
     return data;
   } catch (error) {
-    return error;
+    throw new Error("Internal Server Error [Cloudinary]");
   }
 };
 
@@ -24,7 +24,7 @@ const cloudinaryRemoveImage = async (imagePublicId) => {
     const result = await cloudinary.uploader.destroy(imagePublicId);
     return result;
   } catch (error) {
-    return error;
+    throw new Error("Internal Server Error [Cloudinary]");
   }
 };
 
@@ -34,7 +34,7 @@ const cloudinaryRemoveMultipleImage = async (publicIds) => {
     const result = await cloudinary.v2.api.delete_resources(publicIds);
     return result;
   } catch (error) {
-    return error;
+    throw new Error("Internal Server Error [Cloudinary]");
   }
 };
 
